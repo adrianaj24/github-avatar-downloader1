12c03eeecbc8bffd5899c1d691a9ec16c85600a6
Adriana <ajimenez0024@gmail.com>
2019-04-08 22:49:01 +0000

    downloaded avatars

--- download_avatars.js
+++ download_avatars.js
@@ -7,7 +7,6 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    console.log(pw.GITHUB_TOKEN)
     var options = {
         url:
             `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -20,30 +19,28 @@ function getRepoContributors(repoOwner, repoName, cb) {
         cb(err, data);
     }).auth("adrianaj24", pw.GITHUB_TOKEN);
 
-    }
+}
 
 function downloadImageByURL(url, filePath) {
-    request.get(url)               // Note 1
-        .on('error', function (err) {                                   // Note 2
+    request.get(url)               
+        .on('error', function (err) {                                   
             throw err;
         })
-        .on('response', function (response) {                           // Note 3
+        .on('response', function (response) {                           
             console.log('Response Status Code: ', response.statusMessage);
             console.log('Response Headers:', response.headers['content-type']);
         })
         .pipe(fs.createWriteStream(filePath));
 }    
 
-    console.log('Download complete.');
-// getRepoContributors("jquery", "jquery", function (err, result) {
-//     console.log(result.length)
-//     for (var i = 0; i < result.length; i++) {
-//         console.log(result[i].avatar_url)
-        
-//     }
-//     console.log("Errors:", err)
-// })
+console.log('Download complete.');
+    
+getRepoContributors("jquery", "jquery", function (err, result) {
+    console.log(result.length)
+    for (var i = 0; i < result.length; i++) {
+        downloadImageByURL(result[i].avatar_url, "./avatars/" + result[i].login + ".jpg");
+    }
+    console.log("Errors:", err)
+})
 
     
-
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")
