543501079e2e6350ad7deb7a4e082afa3b05c3ec
Adriana <ajimenez0024@gmail.com>
2019-04-08 21:31:48 +0000

    avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,17 +1,31 @@
 var request = require('request');
+var pw = require("./secrets");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-    request(url, function (err, res, body) {
-        cb(err, body);
-    });
+    console.log(pw.GITHUB_TOKEN)
+    var options = {
+        url: 
+        `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, 
+        headers: {
+        'User-Agent': 'request'
+        }
+    }
+    request(options, function (err, res, body) {
+        var data = JSON.parse(body);
+        cb(err, data);
+    }).auth("adrianaj24", pw.GITHUB_TOKEN);
+
+
 }
- 
 getRepoContributors("jquery", "jquery", function (err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
-});
+    console.log(result.length)
+    for (var i = 0; i < result.length; i++) {
+        console.log(result[i].avatar_url)
+        
+    }
+    console.log("Errors:", err)
+})
