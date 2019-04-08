5e8f57ef49827ada001250cd1acc0181bf5f16c6
Adriana <ajimenez0024@gmail.com>
2019-04-08 22:10:11 +0000

    stored avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var pw = require("./secrets");
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -8,10 +9,10 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
     console.log(pw.GITHUB_TOKEN)
     var options = {
-        url: 
-        `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`, 
+        url:
+            `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
         headers: {
-        'User-Agent': 'request'
+            'User-Agent': 'request'
         }
     }
     request(options, function (err, res, body) {
@@ -19,13 +20,30 @@ function getRepoContributors(repoOwner, repoName, cb) {
         cb(err, data);
     }).auth("adrianaj24", pw.GITHUB_TOKEN);
 
-
-}
-getRepoContributors("jquery", "jquery", function (err, result) {
-    console.log(result.length)
-    for (var i = 0; i < result.length; i++) {
-        console.log(result[i].avatar_url)
-        
     }
-    console.log("Errors:", err)
-})
+
+function downloadImageByURL(url, filePath) {
+    request.get(url)               // Note 1
+        .on('error', function (err) {                                   // Note 2
+            throw err;
+        })
+        .on('response', function (response) {                           // Note 3
+            console.log('Response Status Code: ', response.statusMessage);
+            console.log('Response Headers:', response.headers['content-type']);
+        })
+        .pipe(fs.createWriteStream(filePath));
+}    
+
+    console.log('Download complete.');
+// getRepoContributors("jquery", "jquery", function (err, result) {
+//     console.log(result.length)
+//     for (var i = 0; i < result.length; i++) {
+//         console.log(result[i].avatar_url)
+        
+//     }
+//     console.log("Errors:", err)
+// })
+
+    
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")
